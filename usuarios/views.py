16925066f840aaff548e880fbfc1ebfcbef54405
Renan Costa da Silva <renancs93@gmail.com>
2021-10-04 22:03:31 -0300

    Criação do Projeto. Tela de Cadastro de Usuário

---
+++ usuarios/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Usuario
+from django.shortcuts import redirect
+import hashlib
+
+# Create your views here.
+
+def cadastro(request):
+  status = request.GET.get('status')
+  print(status)
+  return render(request, 'cadastro.html', {'status': status})
+
+
+def login(request):
+   return render(request, 'login.html')
+
+def valida_cadastro(request):
+  nome = request.POST.get('nome')
+  email = request.POST.get('email')
+  senha = request.POST.get('senha')
+
+  ## Validação de email
+  if len(senha) < 6 or len(senha) > 12:
+    return redirect('/auth/cadastro?status=1')
+  
+  if len(nome.strip()) == 0 or len(email.strip()) == 0:
+    return redirect('/auth/cadastro?status=2')
+
+  usuario_existe = Usuario.objects.filter(email = email)
+  if len(usuario_existe) > 0:
+    return redirect('/auth/cadastro?status=3')
+
+  usuario = Usuario(
+    nome = nome,
+    email = email,
+    senha = hashlib.sha256(senha.encode()).hexdigest()
+  )
+
+  try:
+    usuario.save()
+    return redirect('/auth/cadastro?status=0')
+  except:
+    return HttpResponse('Ocorreu um erro interno. Tente novamente mais tarde')
