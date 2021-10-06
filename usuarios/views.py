fae08aa21286d35c24720a90c12aa64172479261
Renan Costa da Silva <renancs93@gmail.com>
2021-10-06 20:47:18 -0300

    Criado Cursos, aulas e home. Add checagem de segurança

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,13 +7,20 @@ import hashlib
 # Create your views here.
 
 def cadastro(request):
+  if request.session.get('usuario'):
+    return redirect('/home/')
+  
   status = request.GET.get('status')
   print(status)
   return render(request, 'cadastro.html', {'status': status})
 
 
 def login(request):
-   return render(request, 'login.html')
+  if request.session.get('usuario'):
+    return redirect('/home/')
+
+  status = request.GET.get('status')
+  return render(request, 'login.html', {'status': status})
 
 def valida_cadastro(request):
   nome = request.POST.get('nome')
@@ -42,3 +49,20 @@ def valida_cadastro(request):
     return redirect('/auth/cadastro?status=0')
   except:
     return HttpResponse('Ocorreu um erro interno. Tente novamente mais tarde')
+
+def valida_login(request):
+  email = request.POST.get('email')
+  senha = request.POST.get('senha')
+
+  senha = hashlib.sha256((senha.encode())).hexdigest()
+  usuarios = Usuario.objects.filter(email = email).filter(senha = senha)
+
+  if len(usuarios) == 0:
+    return redirect('/auth/login?status=1')
+  elif len(usuarios) > 0:
+    request.session['usuario'] = usuarios[0].id
+    return redirect('/home')
+
+def sair(request):
+  request.session.flush()
+  return redirect('/auth/login')
